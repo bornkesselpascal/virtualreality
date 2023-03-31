90ebe365520480de186234680a0bc39132e65f26
Pascal <89606160+fly-spot@users.noreply.github.com>
2023-03-31 15:27:22 +0200

    Blatt 2, Aufgabe 1 bis 5

---
+++ pmmi/Assets/Scripts/FlightSimulator/SwitchScene.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchScene : MonoBehaviour
+{
+    public string scene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(this.transform.rotation.y > 0.99)
+        {
+            Debug.Log("Now I switch the scene...");
+            SceneManager.LoadScene(scene);
+        }
+    }
+}
