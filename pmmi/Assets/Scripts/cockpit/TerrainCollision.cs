b251872db5dea502686d28c075fcb500c71c0ec1
Pascal <89606160+bornkesselpascal@users.noreply.github.com>
2023-04-14 17:00:12 +0200

    Added trigger for terrain collision

---
+++ pmmi/Assets/Scripts/cockpit/TerrainCollision.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainCollision : MonoBehaviour
+{
+    public Transform explosionPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collision Detected");
+
+        ContactPoint contact = collision.contacts[0];
+        Quaternion rotation = Quaternion.FromToRotation(Vector3.up, contact.normal);
+        Vector3 position = contact.point;
+        Instantiate(explosionPrefab, position, rotation);
+        Destroy(gameObject);
+    }
+}
