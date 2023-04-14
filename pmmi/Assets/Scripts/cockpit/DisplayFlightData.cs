a99b321cce30d6bbcf876cbddf8514037eb82e85
Pascal <89606160+bornkesselpascal@users.noreply.github.com>
2023-04-14 12:20:23 +0200

    Added blinking warn image

--- pmmi/Assets/Scripts/cockpit/DisplayFlightData.cs
+++ pmmi/Assets/Scripts/cockpit/DisplayFlightData.cs
@@ -13,6 +13,8 @@ public class DisplayFlightData : MonoBehaviour
     public float warningHeight;
     public GameObject warnImage;
     private FlyJoystick flyJoystick;
+    private bool warnImageState = false;
+    private bool blinkActive = false;
 
     // Start is called before the first frame update
     void Start()
@@ -27,11 +29,42 @@ public class DisplayFlightData : MonoBehaviour
         float airspeed = flyJoystick.speed;
 
         string altInfo = "Alt: " + Math.Round(altitude, 0) + "m";
-        string speedInfo = "Speed: " + Math.Round((airspeed*10 + 200), 0) + "km/h";
+        string speedInfo = "Speed: " + Math.Round((airspeed * 10 + 200), 0) + "km/h";
 
         textHeight.text = altInfo;
         textSpeed.text = speedInfo;
-        
-        warnImage.SetActive((altitude <= warningHeight));
+
+        if (altitude <= warningHeight)
+        {
+            if (!blinkActive)
+            {
+                StopAllCoroutines();
+                StartCoroutine("Blink");
+                textHeight.color = Color.red;
+                blinkActive = true;
+            }
+        }
+        else
+        {
+            if (blinkActive)
+            {
+                StopAllCoroutines();
+                warnImage.SetActive(false);
+                textHeight.color = Color.white;
+                blinkActive = false;
+            }
+        }
+    }
+
+
+    IEnumerator Blink()
+    {
+        while (true)
+        {
+            warnImageState = !warnImageState;
+            warnImage.SetActive(warnImageState);
+
+            yield return new WaitForSeconds(0.5f);
+        }
     }
 }
